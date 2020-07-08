fc0d3d0c7c5b32da2eaaf813cb26af574140a413
LeonardoLucarelli <68016337+LeonardoLucarelli@users.noreply.github.com>
2020-07-08 17:49:12 +0200

    Add files via upload

---
+++ source/3D_triangulation.py
@@ -0,0 +1,111 @@
+import numpy as np
+import h5py
+from os.path import join
+import pandas as pd
+import pickle
+
+h5_dir = r'C:\ '
+dataset_dir = r'C:\ '
+P_mat_dir = join(dataset_dir, 'P_matrices')
+
+# Costants
+H = 260;
+W = 344;
+num_joints = 13
+
+subjects = [13,14,15,16,17]
+sessions = [1,2,3,4,5]
+moves = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6,],[1,2,3,4,5,6,7]]
+
+# Importing projection matrix and camera position for used cameras
+P_mat_cam2 = np.load(join(P_mat_dir,'P2.npy'))
+P_mat_cam3 = np.load(join(P_mat_dir,'P3.npy'))
+cameras_pos = np.load(join(P_mat_dir,'camera_positions.npy'))
+
+# Importing saved max predictions' coordinates
+with open(r'C:\ ', "rb") as fp2:   # Unpickling
+    p_max_coords_cam2_ = pickle.load(fp2)
+with open(r'C:\ ', "rb") as fp3:  # Unpickling
+    p_max_coords_cam3_ = pickle.load(fp3)
+p_max_coords_cam2= np.array(p_max_coords_cam2_)
+p_max_coords_cam3=np.array(p_max_coords_cam3_)
+
+# One of the two point used for triangulation is camera center
+Point0 = (np.stack((cameras_pos[1],cameras_pos[2])))
+
+def project_uv_xyz_cam(uv, M):
+    N = len(uv)
+    uv_homog = np.hstack((uv, np.ones((N, 1))))
+    M_inv= np.linalg.pinv(M)
+    xyz = np.dot(M_inv, uv_homog.T).T
+    x = xyz[:, 0] / xyz[:, 3]
+    y = xyz[:, 1] / xyz[:, 3]
+    z = xyz[:, 2] / xyz[:, 3]
+    return x,y,z
+
+def find_intersection(P0,P1):
+    # generate all line direction vectors
+    n = (P1-P0) / np.linalg.norm(P1-P0,axis=1)[:,np.newaxis] # normalized
+    # generate the array of all projectors
+    projs = np.eye(n.shape[1]) - n[:,:,np.newaxis]*n[:,np.newaxis]  # I - n*n.T
+
+    # generate R matrix and q vector
+    R = projs.sum(axis=0)
+    q = (np.matmul(projs,P0[:,:,np.newaxis])).sum(axis=0)
+    # solve the least squares problem for the
+    # intersection point p: Rp = q
+    p = np.linalg.lstsq(R,q, rcond=None)[0]
+    return p.T
+
+def caluculate_3D_mpjpe(label_3d, pred_3d):
+    mpjpe_3d_joints = np.linalg.norm((label_3d - pred_3d), axis=-1)
+    mpjpe_3d_sample = np.nanmean(mpjpe_3d_joints)
+    return mpjpe_3d_sample
+
+# Iterator variable - increment every new frame to take proper element in p_max_coords variables
+count = 0
+
+mpjpe_3D_mtx=[]
+
+for subj in subjects:
+    for sess in sessions:
+        for mov in moves[sessions.index(sess)]:
+            if (subj == 14 and sess == 5 and mov == 3):
+                mpjpe_3D_mtx.append([])
+            else:
+                # load input and 3D label files
+                datafile = 'S{}_session{}_mov{}_7500events'.format(subj, sess, mov)
+                path_y = join(h5_dir,datafile+'_label.h5')
+                y_h5 = h5py.File(path_y, 'r')
+                frames = y_h5['XYZ'].shape[0]
+                mpjpe_3D = []
+                for frame in range(frames):
+                    # 3D label
+                    label_3d = y_h5['XYZ'][frame].T
+                    # initialize empty sample of 3D prediction
+                    pred_3d = np.zeros(label_3d.shape)
+                    pred_2d_cam2_ = np.zeros(p_max_coords_cam2[count].shape)
+                    pred_2d_cam3_ = np.zeros(p_max_coords_cam3[count].shape)
+                    pred_2d_cam2_[:,0] = p_max_coords_cam2[count][:,1]
+                    pred_2d_cam2_[:,1] = H - p_max_coords_cam2[count][:,0]
+                    pred_2d_cam3_[:,0] = p_max_coords_cam3[count][:,1]
+                    pred_2d_cam3_[:,1] = H - p_max_coords_cam3[count][:,0]
+                    x_cam2_pred, y_cam2_pred, z_cam2_pred = project_uv_xyz_cam(pred_2d_cam2_, P_mat_cam2)
+                    x_cam3_pred, y_cam3_pred, z_cam3_pred = project_uv_xyz_cam(pred_2d_cam3_, P_mat_cam3)
+                    xyz_cam2 = np.stack((x_cam2_pred, y_cam2_pred, z_cam2_pred), axis=1)
+                    xyz_cam3 = np.stack((x_cam3_pred, y_cam3_pred, z_cam3_pred), axis=1)
+                    for joint_idx in range(13):
+                        # coordinates for both cameras of 2nd point of triangulation line
+                        Point1 = np.stack((xyz_cam2[joint_idx,:], xyz_cam3[joint_idx,:]), axis=1).T
+                        intersection = find_intersection(Point0, Point1)
+                        pred_3d[joint_idx] = intersection[0]
+                    mpjpe_3D.append(caluculate_3D_mpjpe(label_3d, pred_3d))
+                    count += 1
+                mpjpe_3D_mtx.append(mpjpe_3D)
+        print('Session{}_subj{}'.format(subj,sess))
+
+# Save 3D mpjpe values (in mm) in excel file with each table's row corresponding to a single move
+df = pd.DataFrame(mpjpe_3D_mtx)
+writer = pd.ExcelWriter(r'C:\Users\aless\Documents\Universita\MAGISTRALE\Computer vision-Deep learning\DVS_HPE_project\mpjpe_output\mpjpe_3D_conf03_row1_nan.xlsx', engine='xlsxwriter')
+df.to_excel(writer,index=False)
+writer.save()
---
+++ source/testing_with_conf.py
@@ -0,0 +1,127 @@
+import tensorflow.keras.backend as k
+from tensorflow.keras.models import load_model
+from os.path import join
+import h5py
+import numpy as np
+import cv2
+import pandas as pd
+import pickle
+
+h5_dir = r'C:\ '
+dataset_dir = r'C:\ '
+P_mat_dir = join(dataset_dir, 'P_matrices')
+
+# Costants
+H = 260;
+W = 344;
+num_joints = 13
+
+subjects = [13,14,15,16,17]
+sessions = [1,2,3,4,5]
+moves = [[1,2,3,4,5,6,7,8],[1,2,3,4,5,6],[1,2,3,4,5,6],[1,2,3,4,5,6,],[1,2,3,4,5,6,7]]
+
+# Cam_id in range [0,1,2,3] corresponds to camera [4,1,3,2]
+cam_id = 3
+
+confidence_threshold = 0.3
+
+
+# Gaussian blur filter
+def decay_heatmap(heatmap, sigma2=4):
+    heatmap = cv2.GaussianBlur(heatmap,(0,0),sigma2)
+    heatmap /= np.max(heatmap) # keep the max to 1
+    return heatmap
+
+# 2D mean square error
+def mse2D(y_true, y_pred):
+    mean_over_ch = k.mean(k.square(y_pred - y_true), axis=-1)
+    mean_over_w = k.mean(mean_over_ch, axis=-1)
+    mean_over_h = k.mean(mean_over_w, axis=-1)
+    return mean_over_h
+
+# Transform Vicon labels to match DVS camera reference system
+def get_2D_label_coords(vicon_xyz, cam_id):
+    # From 3D label, get 2D label coordinates and heatmaps for selected camera
+    if cam_id==1: P_mat_cam = np.load(join(P_mat_dir,'P1.npy'))
+    elif cam_id==3: P_mat_cam = np.load(join(P_mat_dir,'P2.npy'))
+    elif cam_id==2: P_mat_cam = np.load(join(P_mat_dir,'P3.npy'))
+    elif cam_id == 0: P_mat_cam = np.load(join(P_mat_dir, 'P4.npy'))
+    # use homogeneous coordinates representation to project 3d XYZ coordinates to 2d UV pixel coordinates.
+    vicon_xyz_homog = np.concatenate([vicon_xyz, np.ones([1, 13])], axis=0)
+    # vincoXYZ is a 4x13 matrix
+    coord_pix_homog = np.matmul(P_mat_cam, vicon_xyz_homog)
+    coord_pix_homog_norm = coord_pix_homog / coord_pix_homog[-1]
+    u = coord_pix_homog_norm[0]
+    v = H - coord_pix_homog_norm[1]  # flip v coordinate to match the image direction
+    # mask is used to make sure that pixel positions are in frame range.
+    mask = np.ones(u.shape).astype(np.float32)
+    mask[np.isnan(u)] = 0;
+    mask[np.isnan(v)] = 0
+    mask[u > W] = 0;
+    mask[u <= 0] = 0;
+    mask[v > H] = 0;
+    mask[v <= 0] = 0
+    # pixel coordinates
+    u = u.astype(np.int32)
+    v = v.astype(np.int32)
+    return np.stack((v, u), axis=-1), mask
+
+def move_mpjpe_calculation(test_set, label_set, cam_id):
+    # predict with CNN, extract predicted 2D coordinates, and return mpjpe for frames set of a specific move
+    # cropping rightmost pixels
+    frames = test_set.shape[0]
+    mpjpe=[]
+    p_max_coords = np.zeros((num_joints,2))
+    for frame_id in range(frames):
+        # frame_id:frame_id + 1 allow select specific frame and mantain correct shape of predict method's argument
+        test_frame = test_set[frame_id:frame_id + 1, :, :344]
+        prediction = trained_model.predict(np.expand_dims(test_frame, axis=-1))
+        y_2d, gt_mask, y_heatmaps = get_2D_label_coords(label_set['XYZ'][frame_id], cam_id)
+        np.reshape(p_max_coords, y_2d.shape)
+        for j_id in range(y_2d.shape[0]):
+            pred_j_map = prediction[0, :, :, j_id]  # every prediction saved at index 0 since first dimension cardinality not defined
+            # predict max value for each heatmap and keep only the first one if more are present
+            is_all_zero = np.all(p_max_coords[j_id] == 0)
+            if np.max(pred_j_map) >= confidence_threshold or is_all_zero:
+                p_max_coords_tmp = np.argwhere(pred_j_map == np.max(pred_j_map))
+                p_max_coords[j_id] = p_max_coords_tmp[0]
+        y_2d = y_2d.astype(np.float)
+        # where mask is 0, set gt back to NaN
+        y_2d[gt_mask == 0] = np.nan
+        dist_2d = np.linalg.norm((y_2d - p_max_coords), axis=-1)
+        mpjpe.append(np.nanmean(dist_2d))
+        p_coords.append(np.ndarray.tolist(p_max_coords))
+    return mpjpe
+
+# Pretrained model import
+trained_model=load_model(join(dataset_dir,dataset_dir+'\DHP_CNN.model'), custom_objects={'mse2D': mse2D})
+
+mpjpe_mtx=[]
+p_coords=[]
+
+for subj in subjects:
+    for session in sessions:
+        for move in moves[sessions.index(session)]:
+            if (subj == 14 and session == 5 and move == 3):
+                mpjpe_mtx.append([])
+            else:
+                datafile = r'S{}_session{}_mov{}_7500events'.format(subj, session, move)
+                path_x = join(h5_dir, datafile + '.h5')
+                path_y = join(h5_dir, datafile + '_label.h5')
+                x_h5 = h5py.File(path_x, 'r')
+                y_h5 = h5py.File(path_y, 'r')
+                test_set = x_h5['DVS'][:, :, :344, cam_id]
+                mpjpe = move_mpjpe_calculation(test_set, y_h5, cam_id)
+                mpjpe_mtx.append(mpjpe)
+        print('Subj{}_sess{}'.format(subj,session))
+
+# Save mpjpe values in excel file with each table's row corresponding to a single move
+df = pd.DataFrame(mpjpe_mtx)
+writer = pd.ExcelWriter(r'C:\ ', engine='xlsxwriter')
+df.to_excel(writer,index=False)
+writer.save()
+
+# Save max predictions' coordinates in local file
+with open(r'C:\ ', "wb") as fp:   #Pickling
+    pickle.dump(p_coords, fp)
+
